333030f75bdec641865beaa1ad161b3876f280f1
Ynkefn <58092521+Ynkefn@users.noreply.github.com>
2019-11-25 18:49:11 -0600

    Add files via upload

--- Final/Pipe_Class.py
+++ Final/Pipe_Class.py
@@ -7,14 +7,14 @@ from OpenGL.GLUT import *
 from PipeDrawingClass import gl2D, gl2DText, gl2DCircle
 
 class Node:
-    def __init__(self):
+    def __init__(self):         # Node Class holds all node values
         self.name = None
         self.x = None
         self.y = None
         self.z = None
 
 class PipeLink:
-    def __init__(self):
+    def __init__(self):         # Pipe Class holds all pipe values
         self.name = None
         self.begNodeName = None
         self.endNodeName = None
@@ -24,36 +24,37 @@ class PipeLink:
         self.diameter = None
 
 class Source:
-    def __init__(self):
+    def __init__(self):         # Source Class holds all source values
         self.inletNodeID = None
         self.outletNodeID = None
 
-class Reference:
+class Reference:                # Reference Class holds all reference values
     def __init__(self):
         self.nodeID = None
         self.pressure = None
 
-class Devices:
+class Devices:                  # Devices Class holds all device values
     def __init__(self):
         self.deviceID = None
         self.begNodeID = None
         self.endNodeID = None
 
 class PumpData:
-    def __init__(self):
+    def __init__(self):         # PumpData Class holds all pump_data values
         self.pumpID = None
         self.description = None
         self.shutoff_head = None
-        ######################## STOPPED HERE #######################
-        # Need variables for coefficients still
+        self.cCoeff = []
+        self.dCoeff = []
 
-class DeviceData:
+class DeviceData:               # DeviceData Class holds all device_data
     def __init__(self):
-        ## NEED ALL VARIABLES STILL ##
-        self.test = None
+        self.deviceID = None
+        self.description = None
+        self.cCoeff = []
 
 class Pipe:
-    def __init__(self, ):
+    def __init__(self, ):       # Pipe class holds all usable values
         self.title = None
         self.fund_dis_unit = None
         self.fund_force_unit = None
@@ -68,7 +69,7 @@ class Pipe:
         self.roughness = None
         self.density = None
         self.viscosity = None
-        self.node = []
+        self.node = []                  # These lists is where the data from the classes above will be stored
         self.pipes = []
         self.ref_nodes = []
         self.devices = []
@@ -82,7 +83,7 @@ class Pipe:
             cells = line.strip().split(',')
             keyword = cells[0].lower()
 
-            if keyword == 'title': self.title = cells[1].strip()
+            if keyword == 'title': self.title = cells[1].strip()        # Standard keyword single value stuff
             if keyword == 'distance_unit': self.fund_dis_unit = cells[1].strip()
             if keyword == 'force_unit': self.fund_force_unit = cells[1].strip()
             if keyword == 'time_unit': self.fund_time_unit = cells[1].strip()
@@ -104,22 +105,22 @@ class Pipe:
                 self.head_unit_conv = float(cells[2].strip())
 
             if keyword == 'nodes':
-                hold = []
-                for cell in cells[1:]:
+                hold = []       # Declare empty list for holding values
+                for cell in cells[1:]:       # Loop through all cells and remove spaces and parenthesis
                     value = cell.replace("(", "").replace(")", "").strip()
-                    hold.append(value)
-                for i, j in enumerate(hold):
-                    if i % 4 is 0:
-                        thisnode = Node()
-                        thisnode.z = float(hold[i-1])
-                        thisnode.y = float(hold[i-2])
+                    hold.append(value)       # Add every value to hold
+                for i, j in enumerate(hold):    # Loop through hold using enumerate. i is the count (from enumerate)
+                    if i % 4 is 0:              # j is the actual index
+                        thisnode = Node()       # Check for every 4th value by using modulo.
+                        thisnode.z = float(hold[i-1])   # thisnode = Node() creates an instance for Node
+                        thisnode.y = float(hold[i-2])   # Fill every data value in Node with their respective value
                         thisnode.x = float(hold[i-3])
                         thisnode.name = hold[i-4]
-                        self.node.append(thisnode)
+                        self.node.append(thisnode)      # Append all values to the list into the Main Pipe Class
 
             if keyword == 'pipes':
                 hold = []
-                for cell in cells[1:]:
+                for cell in cells[1:]:                  # Exact same as above
                     value = cell.replace("(", "").replace(")", "").strip()
                     hold.append(value)
                 for i, j in enumerate(hold):
@@ -132,7 +133,7 @@ class Pipe:
                         self.pipes.append(thispipe)
 
             if keyword == 'source':
-                thissource = Source()
+                thissource = Source()               # More basic keyword readings
                 thissource.inletNodeID = cells[1].strip()
                 thissource.outletNodeID = cells[2].strip()
                 self.source.append(thissource)
@@ -144,7 +145,7 @@ class Pipe:
                     hold.append(value)
                 for i, j in enumerate(hold):
                     if i % 2 is 0:
-                        thisref = Reference()
+                        thisref = Reference()       # Same as above
                         thisref.nodeID = hold[i-2]
                         thisref.pressure = hold[i-1]
                         self.ref_nodes.append(thisref)
@@ -156,11 +157,30 @@ class Pipe:
                     hold.append(value)
                 for i, j in enumerate(hold):
                     if i % 3 is 0:
-                        thisdevice = Devices()
+                        thisdevice = Devices()      # Same same
                         thisdevice.deviceID = hold[i-3]
                         thisdevice.begNodeID = hold[i-2]
                         thisdevice.endNodeID = hold[i-1]
                         self.devices.append(thisdevice)
+
+            if keyword == 'pump_data':
+                thispData = PumpData()
+                thispData.pumpID = cells[1].strip()     # This one is a little different. Basically the same concepts
+                thispData.description = cells[2].strip()    # as above but I went through each cell and declared the
+                thispData.shutoff_head = float(cells[3].strip())    # single values first. Then went on to the
+                for i in cells[4:7]:                                # coefficients. Made lists for them and appended the
+                    thispData.cCoeff.append(float(i))               # the values to them.
+                for i in cells[8:11]:
+                    thispData.dCoeff.append(float(i))
+                self.pump_data.append(thispData)                    # Appended all data to main list in Pipe Class
+
+            if keyword == 'device_data':
+                thisdData = DeviceData()
+                thisdData.deviceID = cells[1].strip()
+                thisdData.description = cells[2].strip()
+                for i in cells[3:6]:
+                    thisdData.cCoeff.append(float(i))
+                self.device_data.append(thisdData)
 
         self.UpdateConnections()
 
@@ -170,7 +190,7 @@ class Pipe:
         ymin = 99999
         ymax = -99999
 
-        for pipe in self.pipes:
+        for pipe in self.pipes:             # Determine drawing size based off of max and min x and y values
             for node in self.node:
                 if node.name == pipe.begNodeName:
                     pipe.nodeBeg = node
@@ -188,11 +208,48 @@ class Pipe:
         self.DrawingSize = [xmin, xmax, ymin, ymax*1.25]
 
     def DrawPipePicture(self):
-        x = 0
-        y = 0
+        x1, y1, x2, y2 = None, None, None, None     # This draws source lines
+        for source in self.source:                  # Initialize x and y values and loop through source and node
+            for node in self.node:
+                if source.inletNodeID == node.name:     # Check is inlet and outlet node for source equal node names
+                    x1 = node.x                         # If they do, set x and y values to node values
+                    y1 = node.y
+                if source.outletNodeID == node.name:
+                    x2 = node.x
+                    y2 = node.y
+                glColor3f(0, 0, 1)                      # Draw source line
+                glLineWidth(2)
+                glBegin(GL_LINE_STRIP)
+                if x1 or y1 is not None:
+                    glVertex2f(x1, y1)
+                if x2 or y2 is not None:
+                    glVertex2f(x2, y2)
+                glEnd()
+
+            glColor3f(0, 0, 0)
+            name = source.inletNodeID + source.outletNodeID
+            gl2DText(name, x1 + (x2-x1)/2, y1 + (y2-y1)/2)
+
+        for device in self.devices:
+            x1, x2, y1, y2 = None, None, None, None
+            for node in self.node:
+                if device.begNodeID == node.name:       # Same as above but for devices
+                    x1 = int(node.x)
+                    y1 = int(node.y)
+                if device.endNodeID == node.name:
+                    x2 = int(node.x)
+                    y2 = int(node.y)
+                glColor3f(0, 0, 0)
+                glLineWidth(2)
+                glBegin(GL_LINE_STRIP)
+                if x1 and y1 is not None:
+                    glVertex2f(x1, y1)
+                if x2 and y2 is not None:
+                    glVertex2f(x2, y2)
+                glEnd()
 
         for pipe in self.pipes:
-            x1 = pipe.nodeBeg.x
+            x1 = pipe.nodeBeg.x             # Loop through all pipes and draw all pipes
             y1 = pipe.nodeBeg.y
             x2 = pipe.nodeEnd.x
             y2 = pipe.nodeEnd.y
@@ -209,7 +266,7 @@ class Pipe:
             gl2DText(name, x1 + (x2-x1)/2, y1 + (y2-y1)/2)
 
         for node in self.node:
-            red = False
+            red = False                         # Draw all nodes. Including reference nodes.
             radius = (self.DrawingSize[1]-self.DrawingSize[0]) / 60
             for ref in self.ref_nodes:
                 if ref.nodeID == node.name:
